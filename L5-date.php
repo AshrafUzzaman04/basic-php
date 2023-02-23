c462957022746bbc66c4d7253a5de8e2243743e1
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-02-24 00:25:20 +0600

    cut the leap-year-generator project

--- L5-date.php
+++ L5-date.php
@@ -1,12 +1,12 @@
 <?php
 
 // set my time...
-date_default_timezone_set("Asia/Dhaka");  
+date_default_timezone_set("Asia/Dhaka");
 
 $d = date("d/l/F/M/Y");
-$t = date ("h:i A");
+$t = date("h:i A");
 
-echo "<h2>Date : $d</h2>"; 
+echo "<h2>Date : $d</h2>";
 echo "<br>";
 echo "<h2>Time : $t</h2>" . "<br>";
 
@@ -15,18 +15,18 @@ echo "<h2>Time : $t</h2>" . "<br>";
 // make my own time;
 
 // hour min sec month day year...
-$myGfBirthday = mktime(6, 30 , 20 , 3 , 9 , 2023);
+$myGfBirthday = mktime(6, 30, 20, 3, 9, 2023);
 
-$d = date("d/l/F/Y" , $myGfBirthday);  
+$d = date("d/l/F/Y", $myGfBirthday);
 
-echo "<h2>Date : $d</h2>"; 
+echo "<h2>Date : $d</h2>";
 echo "<br>";
 
 
 // strtotime function
 $nextMonday  = strtotime("next monday +2 days +2 years +2 month");
 
-echo date("d/F/Y -l" , $nextMonday) . "<hr>";  
+echo date("d/F/Y -l", $nextMonday) . "<hr>";
 
 // From the next Friday, all Fridays for 2 months are determined through the loop
 $startDate = strtotime("next Friday");
@@ -37,10 +37,20 @@ $endDate = strtotime(" +2 month ", $startDate);
 //     $startDate = strtotime("+1 weeks ", $startDate);
 // }
 
-for ($i=$startDate; $i <=$endDate ; $i ++ ) { 
-    echo date("d/F/Y -l",$i) . "<br>"; 
+for ($i = $startDate; $i <= $endDate; $i++) {
+    echo date("d/F/Y -l", $i) . "<br>";
     $i = strtotime(" +7 days ", $i);
 }
 
 
-?>
+echo "<hr>";
+// my birthday function make
+// hour, minute, second, month, day, year
+$myBirthday = mktime(6, 1, 20, 8, 4, 2004);
+echo date("d/F/Y -l", $myBirthday) . "<br>" . "<hr>";
+$endDate = strtotime("+20 years", $myBirthday);
+echo "<h2 style='color: blue'>20 year week selection in my birthday</h2>";
+while ($myBirthday <= $endDate) {
+    echo date("d/F/Y -l", $myBirthday) . "<br>";
+    $myBirthday = strtotime("+1 year", $myBirthday);
+}
