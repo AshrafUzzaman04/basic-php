b947a047f9af09ff3638a8ccda838be439971001
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-04-30 03:31:08 +0600

    oop constrats added

--- L12-OOP/advance oop.php
+++ L12-OOP/advance oop.php
@@ -1,25 +1,27 @@
 <?php
 
-class helloWorld {
+class helloWorld
+{
     public function __construct($text)
     {
         echo "$text <br>";
     }
 }
 
-$objHW=  new helloWorld("Hellow India");
-$objHW=  new helloWorld("Hellow India");
-$objHW=  new helloWorld("Hellow China");
-$objHW=  new helloWorld("Hellow Japan");
-$objHW=  new helloWorld("Hellow Singapur");
-$objHW=  new helloWorld("Hellow Soudi Arab");
-$objHW=  new helloWorld("Hellow Brazil");
+$objHW =  new helloWorld("Hellow India");
+$objHW =  new helloWorld("Hellow India");
+$objHW =  new helloWorld("Hellow China");
+$objHW =  new helloWorld("Hellow Japan");
+$objHW =  new helloWorld("Hellow Singapur");
+$objHW =  new helloWorld("Hellow Soudi Arab");
+$objHW =  new helloWorld("Hellow Brazil");
 
 
 
 //  <<<<<<<<<<<<<<<<<<<< (::) scope resolution operator in php >>>>>>>>>>>>>>>>>>>>>>>>
 
-trait TraitName {
+trait TraitName
+{
     protected static string $Uname;
     protected static int $uage;
     protected static string $uoccupation;
@@ -27,29 +29,31 @@ trait TraitName {
     protected static string $uDesigner;
 }
 
-class name {
+class name
+{
     use TraitName;
     protected const Maletitle = "Md";
     protected const Femaletitle = "Ms";
     protected function __construct()
-    {                                                                  
+    {
         return "hi";
     }
 }
 
-class ashraf extends name {
-   public static function name(string $Name , int $age , string $occupation, string $gender , bool $designer): string{
-        $titleName = ($gender === "Female" || $gender == "female" || $gender == "Lady")? name::Femaletitle : name::Maletitle;
+class ashraf extends name
+{
+    public static function name(string $Name, int $age, string $occupation, string $gender, bool $designer): string
+    {
+        $titleName = ($gender === "Female" || $gender == "female" || $gender == "Lady") ? name::Femaletitle : name::Maletitle;
         name::$Uname = $Name;
         name::$uage = $age;
         name::$uoccupation = $occupation;
         name::$ugender = $gender;
         name::$uDesigner = $designer;
-        $massage = ($occupation)? "I am a " : null ;
-        $hi = (name::$uDesigner)? "Yes" :  "No";
-        return "<br>" . "Name : " . $titleName. " " .name::$Uname . "<br>" . "Age : " . name::$uage . "<br>" . "Occupation : " . $massage . name::$uoccupation . "<br>" . "Gender : " . name::$ugender . "<br>" . "Web Designer : " . $hi;
-
+        $massage = ($occupation) ? "I am a " : null;
+        $hi = (name::$uDesigner) ? "Yes" :  "No";
+        return "<br>" . "Name : " . $titleName . " " . name::$Uname . "<br>" . "Age : " . name::$uage . "<br>" . "Occupation : " . $massage . name::$uoccupation . "<br>" . "Gender : " . name::$ugender . "<br>" . "Web Designer : " . $hi;
     }
 }
 
-echo ashraf::name("Ashraf Uz Mahim",18,"Teacher","Lady", true);
+echo ashraf::name("Ashraf Uz Mahim", 18, "Teacher", "Lady", true);
